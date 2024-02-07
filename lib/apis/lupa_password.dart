53fb3fc03340a6fb934210eb0161ab012a50b3d5
ramasii <amdramadhani221005@gmail.com>
2024-02-07 20:22:36 +0700

    menambahkan semacam reset password (blm selesai)

--- lib/apis/lupa_password.dart
+++ lib/apis/lupa_password.dart
@@ -4,18 +4,40 @@ import 'package:http/http.dart' as http;
 class lupaPassword {
   String url = "${apiAddress.address}/lupaPass";
 
+  /// untuk membuat kode sementara
   create(String email) async {
     log("start create kode sementara");
     // buat id_kode_s
     String id_kode_s = "${DateTime.now().millisecondsSinceEpoch}ks";
     try {
-      final response = await http.post(Uri.parse("$url?email_user=$email&id_kode_s=$id_kode_s"));
+      final response = await http
+          .post(Uri.parse("$url?email_user=$email&id_kode_s=$id_kode_s"));
       switch (response.statusCode) {
         case 200:
           log("sukses membuat kode sementara");
           return response;
         default:
           log("membuat kode sementara kode ${response.statusCode}: ${response.body}");
+          return response;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  /// untuk mencocokkan email dan kode sementara di database
+  matchingKodeS(String email, String kode_s) async {
+    log("start getByEmailKode: $email - $kode_s");
+    try {
+      final response = await http
+          .get(Uri.parse("$url/matching?email_user=$email&kode_s=$kode_s"));
+
+      switch (response.statusCode) {
+        case 200:
+          log("sukses lupaPass getByEmailKode: ${response.body}");
+          return response;
+        default:
+          log("lupaPass getByEmailKode ${response.statusCode}: ${response.body}");
           return response;
       }
     } catch (e) {
--- lib/pages/lupa_pass_page.dart
+++ lib/pages/lupa_pass_page.dart
@@ -17,7 +17,11 @@ class LupaPasswordPage extends StatefulWidget {
 class _LupaPasswordPageState extends State<LupaPasswordPage> {
   TextEditingController kodeController = TextEditingController();
   TextEditingController emailController = TextEditingController();
+  TextEditingController pass1Controller = TextEditingController();
+  TextEditingController pass2Controller = TextEditingController();
+
   bool isCodeValid = false;
+  bool isPasswordSame = false;
 
   @override
   void initState() {
@@ -60,24 +64,14 @@ class _LupaPasswordPageState extends State<LupaPasswordPage> {
                 const Divider(
                   color: Colors.transparent,
                 ),
-                _emailAndCode(context),
+                // jika isCodeValid maka tampilkan form password baru, jika tidak maka tampilkan email dan kode
+                _crossFadeEmailToPassword(context),
                 const Divider(
                   color: Colors.transparent,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    _tombolKirimKode(context),
-                    // TODO: buat fungsi untuk lanjutkan setelah mengisi kode sementara, kode yang ada di textForm akan dicocokkan dengan database
-                    TextButton(
-                        onPressed: () {
-                          log("klik lanjutkan");
-                          if (emailController.text.trim().isNotEmpty &&
-                              kodeController.text.trim().isNotEmpty) {}
-                        },
-                        child: Text(AppLocalizations.of(context)!.next)),
-                  ],
-                )
+                // TODO: buat tombol 'kembali' yang mengubah isCodeValid jadi false
+                // di sebelahnya buat tombol 'ubah password' jika kedua password controller sama lalu ditekan maka akan mengubah password yang emailnya sama di database
+                _kirimKodeAndLanjutkan(context)
               ],
             ),
           ),
@@ -86,17 +80,145 @@ class _LupaPasswordPageState extends State<LupaPasswordPage> {
     );
   }
 
+  Row _kirimKodeAndLanjutkan(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _tombolKirimKode(context),
+        _tombolLanjutkan(context),
+      ],
+    );
+  }
+
+  /// berisi AnimatedCrossFade, akan menampilkan [_newPasswordForm] atau [_emailAndCode] sesuai kondisi [isCodeValid]
+  AnimatedCrossFade _crossFadeEmailToPassword(BuildContext context) {
+    return AnimatedCrossFade(
+        firstChild: _newPasswordForm(context), // form password baru
+        secondChild: _emailAndCode(context), // form email dan kode
+        crossFadeState: isCodeValid
+            ? CrossFadeState.showFirst // jika isCodeValid == true
+            : CrossFadeState.showSecond, // jika isCodeValid == false
+        duration: const Duration(milliseconds: 300));
+  }
+
+  Column _newPasswordForm(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextFormField(
+          controller: pass1Controller,
+          decoration: InputDecoration(
+              label: Text("Password baru"), icon: Icon(Icons.lock_outline)),
+        ),
+        TextFormField(
+          controller: pass2Controller,
+          decoration: InputDecoration(
+              label: Text("Ulangi password"), icon: Icon(Icons.lock)),
+          validator: (value) {
+            if (value!.trim() != pass1Controller.text.trim()) {
+              isPasswordSame = false;
+              return "Password tidak sama";
+            } else {
+              isPasswordSame = true;
+            }
+          },
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+        ),
+      ],
+    );
+  }
+
+  TextButton _tombolLanjutkan(BuildContext context) {
+    return TextButton(
+        onPressed: () async {
+          log("klik lanjutkan");
+
+          String email = emailController.text.trim();
+          String kode_s = kodeController.text.trim();
+
+          // jika email dan kode_s tidak kosong
+          if (email.isNotEmpty && kode_s.isNotEmpty) {
+            // tampilkan loading
+            _tampilkanLoading(context);
+
+            // ambil respon ke API
+            Response response =
+                await lupaPassword().matchingKodeS(email, kode_s);
+
+            // tutup loading
+            Navigator.pop(context);
+
+            // jika sukses
+            if (response.statusCode == 200) {
+              // jika ketemu
+              if ((jsonDecode(response.body) as List).isNotEmpty) {
+                // ubah response menjadi JSON/List
+                List resBody = jsonDecode(response.body);
+
+                // dapatkan waktu kode sementara ditambahkan
+                DateTime waktuKodeS =
+                    DateTime.parse(resBody[0]['added']).toLocal();
+
+                // dapatkan waktu sekarang
+                DateTime now = DateTime.now().toLocal();
+
+                // hitung selisih
+                Duration selisih = now.difference(waktuKodeS);
+
+                log("now: $now - added: $waktuKodeS");
+                log(selisih.inMinutes.toString());
+                // jika selisih masih dibawah 5 menit (masih berlaku)
+                if (selisih.inMinutes < 5) {
+                  log("kode masih berlaku");
+                  setState(() {
+                    isCodeValid = true;
+                  });
+                }
+                // jika tidak (expired)
+                else {
+                  log("kode sudah expired: ${selisih.inMinutes} menit");
+                  _showInfoDialog(context, "Kode sudah tidak berlaku");
+                }
+              }
+              // jika tidak ketemu
+              else {
+                log("kode tidak ketemu");
+                _showInfoDialog(
+                    context, "Kemungkinan kode salah atau sudah tidak berlaku");
+              }
+            } else {
+              log("lupaPassPage ${response.statusCode}: ${response.body}");
+              _showInfoDialog(context,
+                  "Sepertinya terjadi kesalah di server, coba beberapa saat lagi");
+            }
+          }
+        },
+        child: Text(
+          AppLocalizations.of(context)!.next,
+        ));
+  }
+
+  Future<dynamic> _showInfoDialog(BuildContext context, String msg) {
+    return showCupertinoDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) => AlertDialog(
+              content: Text(msg),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text(AppLocalizations.of(context)!.ok))
+              ],
+            ));
+  }
+
   TextButton _tombolKirimKode(BuildContext context) {
     return TextButton(
         onPressed: () async {
           log("klik kirim kode");
           if (emailController.text.trim().isNotEmpty) {
             // tampilkan loading
-            showCupertinoDialog(
-                context: context,
-                builder: (context) => const Center(
-                      child: CircularProgressIndicator(),
-                    ));
+            _tampilkanLoading(context);
 
             Response response =
                 await lupaPassword().create(emailController.text.trim());
@@ -138,24 +260,43 @@ class _LupaPasswordPageState extends State<LupaPasswordPage> {
             }
           }
         },
-        child: Text(AppLocalizations.of(context)!.sendCode));
+        child: Text(AppLocalizations.of(context)!.sendCode,
+            style: TextStyle(
+                color: emailController.text.trim().isNotEmpty
+                    ? Colors.blue
+                    : Colors.grey)));
   }
 
+  Future<dynamic> _tampilkanLoading(BuildContext context) {
+    return showCupertinoDialog(
+        context: context,
+        builder: (context) => const Center(
+              child: CircularProgressIndicator(),
+            ));
+  }
+
+  /// ini adalah TextFormField untuk email dan kode sementara
   Column _emailAndCode(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         TextFormField(
           controller: emailController,
+          onChanged: (value) => setState(() {
+            print("emailcontroller changed");
+          }),
           decoration: InputDecoration(
               label: Text(AppLocalizations.of(context)!.email),
               icon: const Icon(Icons.mail)),
         ),
         TextFormField(
           controller: kodeController,
+          onChanged: (value) => setState(() {
+            print("kodeController changed");
+          }),
           decoration: InputDecoration(
               label: Text(AppLocalizations.of(context)!.tempCode),
-              icon: const Icon(Icons.lock)),
+              icon: const Icon(Icons.pin)),
         ),
       ],
     );
